87a18534acd40ba6fe86a73cbd58c651e979e826
Boubacar <nboubacar944@gmail.com>
2025-09-20 15:25:48 +0000

    link backend rontend

--- frontend/app/api.ts
+++ frontend/app/api.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+if (!BASE_URL) {
+  throw new Error("NEXT_PUBLIC_API_URL n'est pas défini !");
+}
+
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL + "api/"
-})
-export default api
+  baseURL: `${BASE_URL.replace(/\/$/, "")}/api/`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
